787a383964213343f2785b7aba205ba0f8fa1b9f
Ryo45 <infledad3b2@gmail.com>
2020-04-12 09:21:00 +0900

    add lambda_function

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+import json
+
+body = {'departure':720,'arrive':730,'crowded':80}
+body_2={'departure':730,'arrive':740,'crowded':120}
+body_3={'departure':740,'arrive':750,'crowded':10}
+
+array=[body,body_2,body_3]
+def lambda_handler(event, context):
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': array
+    }
