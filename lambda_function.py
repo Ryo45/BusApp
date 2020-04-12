b7bc73e892fcbe9226b2375c0d86360a67d8812e
Shota Kuroki <shota.kuroki.code@gmail.com>
2020-04-12 16:53:19 +0900

    support parameters on GET

--- lambda_function.py
+++ lambda_function.py
@@ -5,12 +5,26 @@ body_2={'departure':730,'arrive':740,'crowded':120}
 body_3={'departure':740,'arrive':750,'crowded':10}
 
 array=[body,body_2,body_3]
+
 def lambda_handler(event, context):
+    # デバッグ用
+    payload=event
+    
+    # axios.getのparams辞書の中身は'queryStringParameters'で取得する
+    params = event['queryStringParameters']
+    func=params['func']
+    
+    if func=='getBusData':
+        payload = array
+    elif func=='sendReminder':
+        email = params['mailaddress']
+        payload = email
+    
     return {
         'isBase64Encoded': False,
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': '*'
         },
-        'body': json.dumps(array)
+        'body': json.dumps(payload)
     }
--- main.js
+++ main.js
@@ -31,7 +31,11 @@ let app = new Vue({
         },500);
     },
     mounted :function(){
-        axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app')
+        axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app', {
+            params: {
+                func: 'getBusData'
+            }
+        })
         //axios.get('https://3435cwmvwf.execute-api.ap-northeast-1.amazonaws.com/BusApp')
               .then(response => {
                   this.bus_data=response.data
@@ -102,6 +106,20 @@ let app = new Vue({
         post_reminder:function(mailaddress){
             console.log(mailaddress);
             console.log(this.selected_dep_time);
+            
+            axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app', {
+                params: {
+                    func: 'sendReminder',
+                    mailaddress: mailaddress
+                }
+            })
+            .then(response => {
+                console.log('sendReminder response:');
+                console.log(response);
+            })
+            .catch();
+            
+            
             /*axios.post('https://3435cwmvwf.execute-api.ap-northeast-1.amazonaws.com/BusApp/sendReminder',{
                 address: mailaddress,
                 time:this.selected_dep_time
