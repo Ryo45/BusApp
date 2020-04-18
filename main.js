c1e53c85d86851b20a5a71fc4686a6845304fa22
Shota Kuroki <shota.kuroki.code@gmail.com>
2020-04-18 11:37:59 +0900

    Major formatting fix

--- main.js
+++ main.js
@@ -2,48 +2,48 @@
 let app = new Vue({
     el: '#app',
     data: {
-        time_now:{hour:13,min:10},
+        time_now: { hour: 13, min: 10 },
         crowded_now: '120',
-        crowded_next : '120',
+        crowded_next: '120',
         input_time: '13:45',
-        next_time:'13:45',
-        selected_table_index:1000,
-        selected_dep_time:0,
-        isOpen:true,
-        stop_selected:'技本',    
-        show_modal:false,  
-        stop_list:[
+        next_time: '13:45',
+        selected_table_index: 1000,
+        selected_dep_time: 0,
+        isOpen: true,
+        stop_selected: '技本',
+        show_modal: false,
+        stop_list: [
             {
-                name:'技本'
+                name: '技本'
             },
             {
-                name:'10号館'
+                name: '10号館'
             }],
-        recieved_data: [] ,
-        bus_data:[], /* hour: ,min: ,  crowded:  */
-       road_show_1: true,
-       road_show_2:false
+        recieved_data: [],
+        bus_data: [], /* hour: ,min: ,  crowded:  */
+        road_show_1: true,
+        road_show_2: false
     },
-    created:function(station){
-        this.delayFunc_update=_.debounce(this.update_schedule,800);
-        let that=this;
-        this.timer=setInterval(function(){
-            that.road_show_1=!that.road_show_1;
-            that.road_show_2=!that.road_show_2;
-        },500);
+    created: function (station) {
+        this.delayFunc_update = _.debounce(this.update_schedule, 800);
+        let that = this;
+        this.timer = setInterval(function () {
+            that.road_show_1 = !that.road_show_1;
+            that.road_show_2 = !that.road_show_2;
+        }, 500);
     },
-    mounted :function(){
+    mounted: function () {
         axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app', {
-           params: {
+            params: {
                 func: 'getBusData',
                 input_time: this.input_time
             }
         })
-        .then(response => {
-            this.bus_data=response.data // .bodyなし
-            console.log(this.bus_data)
-        })
-        .catch(response => console.log(response));
+            .then(response => {
+                this.bus_data = response.data // .bodyなし
+                console.log(this.bus_data)
+            })
+            .catch(response => console.log(response));
         /*
         axios.get('https://3435cwmvwf.execute-api.ap-northeast-1.amazonaws.com/BusApp')
         .then(response => {
@@ -53,81 +53,74 @@ let app = new Vue({
         .catch(response => console.log(response));
         */
         console.log(this.bus_data);
-        this.time_now.hour=Number(this.bus_data[0].departure/60);
-        this.time_now.min=Number(this.bus_data[0].departure%60);
+        this.time_now.hour = Number(this.bus_data[0].departure / 60);
+        this.time_now.min = Number(this.bus_data[0].departure % 60);
     },
-
-    computed : {
-        localEmail: function(){
+    computed: {
+        localEmail: function () {
             return this.email.split('@')[0].toLowerCase();
         }
     },
-
-    watch:{
-        input_time:function(newValue, oldValue){
+    watch: {
+        input_time: function (newValue, oldValue) {
             this.delayFunc_update();
         }
     },
-
-    methods:{
-        update_schedule: function(station){
+    methods: {
+        update_schedule: function (station) {
             axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app', {
                 params: {
                     func: 'getBusData',
                     input_time: this.input_time
                 }
             })
-            .then(response => { this.bus_data=response.data })
-            .catch(response => console.log(response));
+                .then(response => { this.bus_data = response.data })
+                .catch(response => console.log(response));
         },
-        parse_minutes:function(time_minutes){
-            return (time_minutes/60 | 0)+':' +this.zero_padding(time_minutes%60,2);
+        parse_minutes: function (time_minutes) {
+            return (time_minutes / 60 | 0) + ':' + this.zero_padding(time_minutes % 60, 2);
         },
-        zero_padding:function(num,length){
+        zero_padding: function (num, length) {
             return ('0000000000' + num).slice(-length);
         },
-        selecting_stop:function(name){
-            this.stop_selected=name;
+        selecting_stop: function (name) {
+            this.stop_selected = name;
         },
-        toIcon:function(rate){
-            if (rate<80)
-            {
+        toIcon: function (rate) {
+            if (rate < 80) {
                 return "空き";
-            } else if(rate<130)
-            {
+            } else if (rate < 130) {
                 return "やや混雑";
-            } else{
+            } else {
                 return "混雑";
             }
+        },
+        get_time: function () {
+            this.time_now = new Date();
+        },
+        get_time_from: function () {
 
         },
-        get_time:function(){
-            this.time_now=new Date();
+        activate_td: function (time) {
+            this.selected_dep_time = time;
         },
-        get_time_from:function(){
-            
-            
-        },
-        activate_td:function(time){
-            this.selected_dep_time=time;
-        },
-        post_reminder:function(mailaddress){
+        post_reminder: function (mailaddress) {
             console.log(mailaddress);
             console.log(this.selected_dep_time);
-            
+
             axios.get('https://m3e1tfriu4.execute-api.ap-northeast-1.amazonaws.com/BusApp/app', {
                 params: {
                     func: 'sendReminder',
                     mailaddress: mailaddress
                 }
             })
-            .then(response => {
-                console.log('sendReminder response:');
-                console.log(response);
-            })
-            .catch();
+                .then(response => {
+                    console.log('sendReminder response:');
+                    console.log(response);
+                })
+                .catch();
             this.toggle_modal();
-            
+
             /*axios.post('https://3435cwmvwf.execute-api.ap-northeast-1.amazonaws.com/BusApp/sendReminder',{
                 address: mailaddress,
                 time:this.selected_dep_time
@@ -139,12 +132,8 @@ let app = new Vue({
                 console.log(error);
             });*/
         },
-        toggle_modal:function(){
-            this.show_modal=!this.show_modal;
+        toggle_modal: function () {
+            this.show_modal = !this.show_modal;
         }
-
-    
-
     }
-    
 });
